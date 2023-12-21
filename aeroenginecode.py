1bbdbff065d8113d136b70107f2a5815402d57de
timokramer00 <timokramer01@gmail.com>
2023-12-21 11:57:04 +0100

    fuel mass flow calcs

--- aeroenginecode.py
+++ aeroenginecode.py
@@ -12,7 +12,14 @@ calvalue= 48*HfCO2 + 46*HfH2O - 4*HfC12H23 - 81*HfO2
 #Values at different thrust levels
 #20 kN, 40 kN, 80 kN, 120 kN 
 
-mdot=[10,16,29,39] #kg/s
+mdot3=[10,16,29,39] #kg/s
 P3=[700e3,1100e3,2200e3,3200e3] #Pa
 T3=[540,610,725,820] #K
 T4=[1150,1230,1410,1630] #K
+
+cceff=0.995
+cp_gas=1150
+
+mdot=[]
+for i in range(4):
+    mdot.append((mdot3[i]*cp_gas*(T4[i]-T3[i]))/(cceff*abs(calvalue)))
