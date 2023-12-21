0d0e5aaf4410acc8ec92fbbaa8c575dcfb80ffb5
timokramer00 <timokramer01@gmail.com>
2023-12-21 11:47:11 +0100

    added plot values thrust levels

--- aeroenginecode.py
+++ aeroenginecode.py
@@ -8,3 +8,11 @@ HfCO2=-393.5e3 #J/mole
 HfH2O=-241.8e3 #J/mole
 
 calvalue= 48*HfCO2 + 46*HfH2O - 4*HfC12H23 - 81*HfO2
+
+#Values at different thrust levels
+#20 kN, 40 kN, 80 kN, 120 kN 
+
+mdot=[10,16,29,39] #kg/s
+P3=[700e3,1100e3,2200e3,3200e3] #Pa
+T3=[540,610,725,820] #K
+T4=[1150,1230,1410,1630] #K
