7cc8809b613aefb8c15404f254fb91087eebfbc7
OJunius <oscar@junius.nl>
2024-01-15 17:51:36 +0100

    i forgor

--- combCalcs.py
+++ combCalcs.py
@@ -1,15 +1,46 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from polyLib import CO2, water, N2, fuel
+from polyLib import CO2, water, N2, fuel, O2
 from dataclasses import dataclass
 
 
 class Massflows:
-    O2: float
-    CO2: float
-    fuel: float
-    N2: float
-    water: float
+    O2 = 0
+    CO2 = 0
+    fuel = 0
+    N2 = 0
+    water = 0
+    
+    def __str__(self):
+        r = f"O2 {self.O2 :.3f}, \nCO2 {self.CO2 :.3f}, \nfuel {self.fuel :.3f}, \nN2 {self.N2 :.3f}, \nwater {self.water :.3f} \n \n"
+        return r
+              
+              
+
+    molar_mass = {
+        "CO2": (12.011 + 15.99*2) * 1e-3,
+        "H2O": (1.0078*2 + 15.99) * 1e-3,
+        "fuel": (12*12.011 + 23 * 1.0078) * 1e-3,  # c12h23
+        "O2": (15.99*2) * 1e-3,
+        "N2": 28.0134*1e-3
+        }
+
+
+def massflows_to_moles_funcs(mdots: Massflows):
+    moles = np.array([mdots.O2, mdots.CO2, mdots.fuel, mdots.N2, mdots.water])
+    funcs = [O2, CO2, fuel, N2, water]
+    mole_masses = np.array([molar_mass["O2"], molar_mass["CO2"], molar_mass["fuel"], molar_mass["N2"], molar_mass["H2O"]])
+    moles /= mole_masses
+    return moles, funcs
+
+
+def compute_enthalpy(massflows, temp, ref_temp=298):
+    component_moles, component_funcs = massflows_to_moles_funcs(massflows)
+    total_enth = 0
+    for c_moles, c_func in zip(component_moles, component_funcs):
+        total_enth += c_moles * (c_func.enthalpy(temp) - c_func.enthalpy(ref_temp))
+    return total_enth
+        
 
 
 class Combustor:
@@ -18,13 +49,14 @@ class Combustor:
         self.cp_air = 1000  # J/kg
         self.FAR_stoich = 0.682
         self.fuel_temp = 300  # K
-        self.calvalue = (48/4 * form_enthalpy["CO2"] + 46/4 * form_enthalpy["H2O"] - form_enthalpy["fuel"]) / molar_mass["fuel"]  # J/kg
+        self.calvalue = 81/4 * form_enthalpy["CO2"] + 48/4 * form_enthalpy["H2O"] - form_enthalpy["fuel"]  # J/mol
         self.eng_spec = eng_spec
         self.molar_mass = molar_mass
         self.volume = volume  # m3
-        self.ox_frac = 0.2
+        self.ox_frac = 0.21
+        self.n_iter = 2500
         self.comb_eff = combustion_eff
-        self.mdots = Massflows(0, 0, 0, 0, 0)
+        self.mdots = None
         
         x3 = 0.42 - x1 - x2 - 0.1
         self.zonal_admittance = [0.16, 2*x1, 2*x2, 2*x3 + 0.2] 
@@ -36,7 +68,7 @@ class Combustor:
         plt.grid()
         plt.xlabel("Axial position [m]")
         plt.ylabel("Temperature [K]")
-        for thrust in self.eng_spec["thrust"]:
+        for thrust in self.eng_spec["thrust"][:1]:
             temps = self.compute_temp_total_equiv(thrust)
             print("temps", temps)
             print("stations", self.stations)
@@ -45,47 +77,89 @@ class Combustor:
         plt.show()
         
     def compute_temp_total_equiv(self, thrust):
+        self.mdots = Massflows()
         air_in, press_in, temp_in, temp_out, self.mdots.fuel = self.get_conditions(thrust)
         
         temp = temp_in
         temperatures = [temp]
         
-        for airfrac in self.zonal_admittance:
+        for i, airfrac in enumerate(self.zonal_admittance):
             # get added air
             self.mdots.N2 += (1 - self.ox_frac) * airfrac * air_in
-            self.mdot.O2 = self.ox_frac airfrac * air_in
+            self.mdots.O2 += self.ox_frac * airfrac * air_in
             
-            temp = self.compute_burn(temp)
+            temp = self.compute_burn_equiv_ratio(temp)
             
             temperatures.append(temp)
-        #TODO implement equivalence ratio
         return temperatures
     
-    def compute_burn(self, mdot_O2, mdot_fuel, temp_in):
-        moles_fuel = mdot_fuel / self.molar_mass["fuel"]
-        moles_ox = mdot_O2 / self.molar_mass["O2"]
+    def compute_burn_equiv_ratio(self, reactant_temp):
+        moles_fuel = self.mdots.fuel / self.molar_mass["fuel"]
+        moles_ox = self.mdots.O2 / self.molar_mass["O2"]
         # mol ratios
         o2_per_fuel = 81/4
-        CO2_per_fuel = 0
-        water_per_fuel = 0
+        CO2_per_fuel = 48/4
+        water_per_fuel = 81/4
         
+        products = Massflows()  # inits to zero
+        # figure out released energy and products
+        print(self.mdots)
         
-        # TODO implement iterative solver
         # fuel rich
         if moles_ox/moles_fuel <= o2_per_fuel:
-            # compute new mole values
-            pass
+            print("fuel rich")
+            # all O2 gone, so stays zero kg
+            fuel_burn = moles_ox / o2_per_fuel
+            products.CO2 = fuel_burn * CO2_per_fuel * self.molar_mass["CO2"]
+            products.water = fuel_burn * water_per_fuel * self.molar_mass["H2O"]
+            products.N2 = self.mdots.N2
+            products.fuel = self.mdots.fuel - fuel_burn * self.molar_mass["fuel"]
+            
+            energy_released = fuel_burn * self.calvalue
+            
         
         # ox rich
         else:
-            pass
-                    
-                             
-        return mdot_fuel, new_co2, new_water, mdot_O2, product_temp
-    
-    def calculate_temperature(self, temperature, current_air_mass, new_air, energy_in, temp_in):
+            print("ox rich")
+            fuel_burn = moles_fuel
+            products.O2 = fuel_burn * o2_per_fuel * self.molar_mass["O2"]
+            products.CO2 = fuel_burn * CO2_per_fuel * self.molar_mass["CO2"]
+            products.water = fuel_burn * water_per_fuel * self.molar_mass["H2O"]
+            products.N2 = self.mdots.N2
+            products.fuel = self.mdots.fuel - fuel_burn * self.molar_mass["fuel"]
+            
+            energy_released = fuel_burn * self.calvalue
         
-        return
+        
+        # determine temperature iteratively
+        print(products)
+        reactant_enthalpy = compute_enthalpy(self.mdots, reactant_temp)
+        target = reactant_enthalpy - energy_released
+        temp_prod = reactant_temp
+    
+        running = True
+        n_iter = 0
+        while running:
+            product_enthalpy = compute_enthalpy(products, temp_prod)
+            
+            rel = product_enthalpy / target
+            
+            if abs(rel - 1) < 1e-3:
+                running = False
+            elif rel < 1:
+                temp_prod += 1
+            elif rel > 1:
+                temp_prod -= 1
+            #print(rel)
+            #print(temp_prod)
+            n_iter += 1
+            if n_iter > self.n_iter:
+                raise ValueError("Max number of iterations exceeded!")
+        
+        print()
+        self.mdots = products
+        return temp_prod
+    
     
     def add_to_plot(self, thrust, temperature, total_equivalence_ratio):
         # TODO modify to add equiv ratio
@@ -127,4 +201,4 @@ if __name__ == "__main__":
         "O2": 0
         }
     comb = Combustor(form_enthalpy, eng_spec, molar_mass)
-    comb.plot_temp_distr_all_thrust()
+    comb.plot_all()
--- polyLib.py
+++ polyLib.py
@@ -8,8 +8,11 @@ class MultiShomate:
         self.temp_range = temp_range
     
     def enthalpy(self, temperature):
+        """
+        Takes temperature in Kelvin, returns enthalpy in J/mol
+        """
         # figure out which temp range we're in
-        poly_num = int(np.where(temperature < self.temp_range)[0][0]) - 1
+        poly_num = int(np.where(temperature < self.temp_range)[0][-1]) - 1
         c = self.coeffs[poly_num]
         t = temperature/1000
         # 0 1 2 3 4 5 6 7
@@ -17,6 +20,9 @@ class MultiShomate:
         return (c[0] * t + c[1] * t**2 / 2 + c[2] * t ** 3 / 3 + c[3] * t**4 / 4 - c[4]/t + c[5] - c[7]) * 1e3
     
     def cp(self, temperature):
+        """
+        Takes temperature in Kelvin, returns Cp in J/mol/K
+        """
         # figure out which temp range we're in
         poly_num = int(np.where(temperature < self.temp_range)[0][0]) - 1
         c = self.coeffs[poly_num]
@@ -34,7 +40,7 @@ class NASAPoly:
         self.c_hi = polyCoeff[:7]
         self.c_lo = polyCoeff[7:]
         self.temp_range = temp_range
-        self.R = 8314  # universal gas constant
+        self.R = 8.314  # universal gas constant
         
     def cp(self, temperature):
         """
@@ -45,23 +51,33 @@ class NASAPoly:
         return self.R * (c[0] + c[1] * t + c[2] * t ** 2 + c[3] * t ** 3 + c[4] * t ** 4)
     
     def enthalpy(self, temperature):
+        """
+        returns enthalpy in J/mol/K
+        """
         t = temperature
         c = self._get_poly(temperature)
         return self.R * t * (c[0] + c[1] * t / 2 + c[2] * t ** 2 / 3 + c[3] * t **3 /4 + c[4] * t **4 / 5 + c[5] / t)
         #pass
         
     def _get_poly(self, temp):         
-        poly_num = int(np.where(temp< self.temp_range)[0][0]) - 1
+        poly_num = int(np.where(temp < self.temp_range)[0][0]) - 1
         if poly_num == 0: # low range
             c = self.c_lo
         elif poly_num == 1: # high range
             c = self.c_hi
+        else:
+            print(f"que? {poly_num}")
         return c
         
 
 # obtained from https://webbook.nist.gov/cgi/cbook.cgi?ID=C124389&Units=SI&Mask=1&Type=JANAFG&Table=on#JANAFG
 CO2_ShoCoeff = np.array([[24.99735, 55.18696, -33.69137, 7.948387, -0.136638, -403.6075, 228.2431, -393.5224],[58.16639, 2.720074, -0.492289, 0.038844, -6.447293, -425.9186, 263.6125, -393.5224]])
 CO2_TempRange = np.array([298, 1200, 6000])
+
+O2_ShoCoeff = np.array([[31.32234, -20.23531, 57.86644, -36.50624, -0.007374, -8.903471, 246.7945, 0.0], 
+                        [30.03235, 8.772972, -3.988133, 0.788313, -0.741599, -11.32468, 236.1663, 0], 
+                        [20.91111, 10.72071, -2.020498, 0.146449, 9.245722, 5.337651, 237.6185, 0]])
+O2_TempRange = np.array([100, 700, 2000, 6000])
 
 H2O_ShoCoeff = np.array([[30.092, 6.832514, 6.793435, -2.534480, 0.082139, -250.8810, 223.3967, -241.8264], [41.96426, 8.622053, -1.499780, 0.098119, -11.15764, -272.1797, 219.7809, -241.8264]])
 H2O_TempRange = np.array([500, 1700, 6000])
@@ -74,9 +90,10 @@ N2_TempRange = np.array([500, 2000, 6000])
 # Values from: https://web.stanford.edu/group/haiwanglab/HyChem/approach/Report_Jet_Fuel_Thermochemical_Properties_v6.pdf
 fuel_NASACoeff = np.array([0.20115053E+02, 0.69181815E-01, -0.23171997E-04, 0.19519781E-08, 0.27041864E-12, -0.42549684E+05, -0.79951393E+02, 0.40264606E-01, 0.88749342E-01, 0.42724278E-04,
 -0.11716354E-06, 0.53996904E-10, -0.35190535E+05, 0.32478203E+02])
-fuel_TempRange = np.array([298, 1000, 3000])
+fuel_TempRange = np.array([298, 1000, 9000])
 
 CO2 = MultiShomate(CO2_ShoCoeff, CO2_TempRange)
+O2 = MultiShomate(O2_ShoCoeff, O2_TempRange)
 water = MultiShomate(H2O_ShoCoeff, H2O_TempRange)
 N2 = MultiShomate(N2_ShoCoeff, N2_TempRange)
 fuel = NASAPoly(fuel_NASACoeff, fuel_TempRange)
@@ -88,6 +105,7 @@ if __name__ == "__main__":
         #print(CO2.enthalpy(1201))
         #print(CO2.enthalpy(1200))
         
+        
         molar_mass = {  # kg/mol
             "CO2": (12.011 + 15.99*2) * 1e-3,
             "H2O": (1.0078*2 + 15.99) * 1e-3,
@@ -97,7 +115,6 @@ if __name__ == "__main__":
         }
         
         import matplotlib.pyplot as plt
-        #print(fuel.enthalpy(300)/molar_mass["fuel"])
         
         def vectorize(func, arg):
             out = []
@@ -106,11 +123,11 @@ if __name__ == "__main__":
             return out
         
         
-        t1 = np.linspace(300, 1000, 500)
-        t2 = np.linspace(1000, 2999, 500)
+        t1 = np.linspace(300, 2999, 500)
+        t2 = np.linspace(1000, 2999, 500) 
         
-        cp1 = vectorize(fuel.cp, t1)
-        cp2 = vectorize(fuel.cp, t2)
+        cp1 = vectorize(fuel.enthalpy, t1)
+        cp2 = vectorize(fuel.enthalpy, t2)
         plt.plot(t1, cp1)
         plt.plot(t2, cp2)
         plt.grid()
