1c450dd2f3ead1620da5e244ed11cb04911e628f
timokramer00 <timokramer01@gmail.com>
2024-01-15 16:27:32 +0100

    added ER zones

--- aeroenginecode.py
+++ aeroenginecode.py
@@ -7,7 +7,7 @@ HfO2=0 #J/mole
 HfCO2=-393.5e3 #J/mole
 HfH2O=-241.8e3 #J/mole
 
-calvalue= 48*HfCO2 + 46*HfH2O - 4*HfC12H23 - 81*HfO2
+calvalue= (12*HfCO2 + 46/4*HfH2O - HfC12H23)/0.1673114
 
 #Values at different thrust levels
 #20 kN, 40 kN, 80 kN, 120 kN 
@@ -16,6 +16,7 @@ mdot3=[10,16,29,39] #kg/s
 P3=[700e3,1100e3,2200e3,3200e3] #Pa
 T3=[540,610,725,820] #K
 T4=[1150,1230,1410,1630] #K
+perc_air_zones=[0.16+0.14,0.2+0.16]
 
 cceff=0.995
 cp_gas=1150
@@ -28,11 +29,14 @@ for i in range(4):
 #Equivalence Ratio Calcs
 FAR_stoich_ker=0.0682 #READER
 ER=[]
+ER_PZ=[]
+ER_SZ=[]
 rho3=[]
 R=287
 A=2*np.pi*0.255*0.25
 V=[]
 res_time=[]
+
 
 for i in range(4):
     FAR_actual=mdot[i]/mdot3[i]
@@ -41,10 +45,15 @@ for i in range(4):
     V.append(mdot3[i]/(rho3[i]*A))
     res_time.append(0.25/V[i])
 
+mdot_air_PZ=[]
+mdot_air_SZ=[]
 
 #Equivalence Ratio Calcs (In zones)
 
-print(calvalue)
-print(mdot)
-print(ER)
+for i in range(4):
+    ER_PZ.append(ER[i]/perc_air_zones[0])
+    ER_SZ.append(ER[i]/(perc_air_zones[0]+perc_air_zones[1]))
+
 print(res_time)
+print(ER)
+
